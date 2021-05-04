61c3353ffcabba2f6523ae262c568706d25c2eea
Ma-Vin <ma.vin.github@gmail.com>
2021-05-04 21:52:07 +0200

    feature: abstract service for repository interactions

---
+++ src/main/java/de/ma_vin/ape/users/service/AbstractRepositoryService.java
@@ -0,0 +1,335 @@
+package de.ma_vin.ape.users.service;
+
+import de.ma_vin.ape.users.model.gen.dao.IIdentifiableDao;
+import de.ma_vin.ape.users.model.gen.domain.IIdentifiable;
+import de.ma_vin.ape.utils.generators.IdGenerator;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+@Log4j2
+public abstract class AbstractRepositoryService {
+
+    /**
+     * Adds a child to a parent if its not a direct connection
+     *
+     * @param parentIdentification    identification of the parent
+     * @param childIdentification     identification of the child
+     * @param parentClassName         simple name of parent class
+     * @param childClassName          simple name of child class
+     * @param parentPrefix            id prefix of parent
+     * @param childPrefix             id prefix of child
+     * @param parentRepository        repository of the parent
+     * @param childRepository         repository of the child
+     * @param parentToChildRepository repository of the connection between parent and child
+     * @param connectionCreator       Functional to create the connection. Setting values included
+     * @param <P>                     The parent
+     * @param <S>                     The child
+     * @param <T>                     The connection
+     * @param <I>                     The Id of the connection at repository
+     * @return {@code true} if the child was added to the parent. Otherwise {@code false}
+     */
+    protected <P extends IIdentifiableDao, S extends IIdentifiableDao, T, I extends Serializable>
+    boolean add(String parentIdentification, String childIdentification, String parentClassName, String childClassName
+            , String parentPrefix, String childPrefix
+            , JpaRepository<P, Long> parentRepository, JpaRepository<S, Long> childRepository, JpaRepository<T, I> parentToChildRepository
+            , ConnectionCreator<P, S, T> connectionCreator) {
+
+        log.debug("Add a {} with identification \"{}\" to {} with identification \"{}\""
+                , childClassName, childIdentification, parentClassName, parentIdentification);
+
+        Optional<P> parentDao = parentRepository.findById(IdGenerator.generateId(parentIdentification, parentPrefix));
+        if (parentDao.isEmpty()) {
+            log.error("There is not any {} with identification \"{}\" where to add {} with identification \"{}\""
+                    , parentClassName, parentIdentification, childClassName, childIdentification);
+            return false;
+        }
+
+        Optional<S> childDao = childRepository.findById(IdGenerator.generateId(childIdentification, childPrefix));
+        if (childDao.isEmpty()) {
+            log.error("There is not any {} with identification \"{}\" which could be add {} with identification \"{}\""
+                    , childClassName, childIdentification, parentClassName, parentIdentification);
+            return false;
+        }
+
+        T connection = connectionCreator.create(parentDao.get(), childDao.get());
+
+        T stored = parentToChildRepository.save(connection);
+        if (stored == null) {
+            log.error("{} with identification \"{}\" was not added to {} with identification \"{}\""
+                    , childClassName, childIdentification, parentClassName, parentIdentification);
+            return false;
+        }
+
+        log.debug("{} with identification \"{}\" was added to {} with identification \"{}\""
+                , childClassName, childIdentification, parentClassName, parentIdentification);
+        return true;
+    }
+
+    /**
+     * Searches for a domain object
+     *
+     * @param identification  Identification of the domain object which is searched for
+     * @param idPrefix        prefix to generate ID
+     * @param domainClassName Simple name of the domain class
+     * @param domainConverter Functional to create the domain from the dao object
+     * @param repository      repository where to search at
+     * @param <T>             Domain class of the object which should be searched for
+     * @param <S>             Corresponding dao of the domain class
+     * @return search result
+     */
+    protected <T extends IIdentifiable, S extends IIdentifiableDao> Optional<T> find(String identification, String idPrefix
+            , String domainClassName, DomainConverter<T, S> domainConverter, JpaRepository<S, Long> repository) {
+
+        Long id = IdGenerator.generateId(identification, idPrefix);
+        log.debug("search for {} with identification {} and id {}", domainClassName, identification, id);
+        Optional<S> daoObject = repository.findById(id);
+        if (daoObject.isPresent()) {
+            log.debug("{} with identification {} and id {} was found", domainClassName, identification, id);
+            return Optional.of(domainConverter.convert(daoObject.get()));
+        }
+        log.debug("{} with identification {} and id {} was not found", domainClassName, identification, id);
+        return Optional.empty();
+    }
+
+    /**
+     * Removes a child from a parent if its not a direct connection
+     *
+     * @param parentIdentification Identification of the parent
+     * @param childIdentification  Identification of the state
+     * @param parentClassName      simple name of parent class
+     * @param childClassName       simple name of child class
+     * @param parentPrefix         id prefix of parent
+     * @param childPrefix          id prefix of child
+     * @param parentCreator        Functional to create an empty parent dao
+     * @param childCreator         Functional to create an empty child dao
+     * @param deleter              Function to delete the connection with parent and child dao
+     * @param <P>                  The parent
+     * @param <S>                  The child
+     * @return {@code true} if the child was removed from the parent. Otherwise {@code false}
+     */
+    protected <P extends IIdentifiableDao, S extends IIdentifiableDao>
+    boolean remove(String parentIdentification, String childIdentification, String parentClassName, String childClassName
+            , String parentPrefix, String childPrefix, DaoCreator<P> parentCreator, DaoCreator<S> childCreator
+            , ConnectionDeleter<P, S> deleter) {
+
+        Long parentId = IdGenerator.generateId(parentIdentification, parentPrefix);
+        Long childId = IdGenerator.generateId(childIdentification, childPrefix);
+        log.debug("Remove {} with identification \"{}\" and id {} from {} with identification \"{}\" and id {}"
+                , childClassName, childIdentification, childId, parentClassName, parentIdentification, parentId);
+
+        P parentDao = parentCreator.create();
+        parentDao.setId(parentId);
+        S childDao = childCreator.create();
+        childDao.setId(childId);
+
+        Long numDeleted = deleter.delete(parentDao, childDao);
+        switch (numDeleted.intValue()) {
+            case 0:
+                log.warn("The {} with identification \"{}\" and id {} was not removed from {} with identification \"{}\" and id {}"
+                        , childClassName, childIdentification, childId, parentClassName, parentIdentification, parentId);
+                return false;
+            case 1:
+                log.debug("The {} with identification \"{}\" and id {} was removed from {} with identification \"{}\" and id {}"
+                        , childClassName, childIdentification, childId, parentClassName, parentIdentification, parentId);
+                return true;
+            default:
+                log.error("{} {} with identification \"{}\" and id {} were removed from {} with identification \"{}\" and id {}"
+                        , numDeleted, childClassName, childIdentification, childId, parentClassName, parentIdentification, parentId);
+                return false;
+        }
+    }
+
+    /**
+     * @param domainObject         Domain object which should be stored
+     * @param parentIdentification identification of the parent domain object
+     * @param parentDaoCreator     Functional to create an empty parent Dao
+     * @param daoConverter         Functional to to convert the domain object to a dao one
+     * @param domainConverter      Functional to convert the dao object to a domain one
+     * @param repository           Repository where to store the object
+     * @param <T>                  Domain class of the object which should be stored
+     * @param <S>                  Corresponding dao of the domain class
+     * @param <P>                  Dao class of the parent
+     * @return Stored domain object with additional generated ids, if missing before.
+     * <br>
+     * In case of not existing domain object for given identification, the result will be {@link Optional#empty()}
+     */
+    protected <T extends IIdentifiable, S extends IIdentifiableDao, P extends IIdentifiableDao> Optional<T> save(T domainObject
+            , String parentIdentification
+            , DaoCreator<P> parentDaoCreator
+            , DaoConverter<T, S, P> daoConverter
+            , DomainConverter<T, S> domainConverter
+            , JpaRepository<S, Long> repository) {
+        return save(domainObject
+                , parentIdentification
+                , d -> ""
+                , parentDaoCreator
+                , daoConverter
+                , domainConverter
+                , repository
+                , (dao, storedDao) -> dao);
+    }
+
+    /**
+     * @param domainObject         Domain object which should be stored
+     * @param parentIdentification identification of the parent domain object
+     * @param parentDaoCreator     Functional to create an empty parent Dao
+     * @param daoConverter         Functional to to convert the domain object to a dao one
+     * @param domainConverter      Functional to convert the dao object to a domain one
+     * @param repository           Repository where to store the object
+     * @param adoption             Functional to take over values from persisted dao before saving
+     * @param <T>                  Domain class of the object which should be stored
+     * @param <S>                  Corresponding dao of the domain class
+     * @param <P>                  Dao class of the parent
+     * @return Stored domain object with additional generated ids, if missing before.
+     * <br>
+     * In case of not existing domain object for given identification, the result will be {@link Optional#empty()}
+     */
+    protected <T extends IIdentifiable, S extends IIdentifiableDao, P extends IIdentifiableDao> Optional<T> save(T domainObject
+            , String parentIdentification
+            , DaoCreator<P> parentDaoCreator
+            , DaoConverter<T, S, P> daoConverter
+            , DomainConverter<T, S> domainConverter
+            , JpaRepository<S, Long> repository
+            , Adoption<S> adoption) {
+        return save(domainObject
+                , parentIdentification
+                , d -> ""
+                , parentDaoCreator
+                , daoConverter
+                , domainConverter
+                , repository
+                , adoption);
+    }
+
+    /**
+     * @param domainObject         Domain object which should be stored
+     * @param parentIdentification identification of the parent domain object
+     * @param nameable             Functional to access the name of t
+     * @param parentDaoCreator     Functional to create an empty parent Dao
+     * @param daoConverter         Functional to to convert the domain object to a dao one
+     * @param domainConverter      Functional to convert the dao object to a domain one
+     * @param repository           Repository where to store the object
+     * @param <T>                  Domain class of the object which should be stored
+     * @param <S>                  Corresponding dao of the domain class
+     * @param <P>                  Dao class of the parent
+     * @return Stored domain object with additional generated ids, if missing before.
+     * <br>
+     * In case of not existing domain object for given identification, the result will be {@link Optional#empty()}
+     */
+    protected <T extends IIdentifiable, S extends IIdentifiableDao, P extends IIdentifiableDao> Optional<T> save(T domainObject
+            , String parentIdentification
+            , Nameable<T> nameable
+            , DaoCreator<P> parentDaoCreator
+            , DaoConverter<T, S, P> daoConverter
+            , DomainConverter<T, S> domainConverter
+            , JpaRepository<S, Long> repository) {
+        return save(domainObject
+                , parentIdentification
+                , nameable
+                , parentDaoCreator
+                , daoConverter
+                , domainConverter
+                , repository
+                , (dao, storedDao) -> dao);
+    }
+
+    /**
+     * @param domainObject         Domain object which should be stored
+     * @param parentIdentification identification of the parent domain object
+     * @param nameable             Functional to access the name of t
+     * @param parentDaoCreator     Functional to create an empty parent Dao
+     * @param daoConverter         Functional to to convert the domain object to a dao one
+     * @param domainConverter      Functional to convert the dao object to a domain one
+     * @param repository           Repository where to store the object
+     * @param adoption             Functional to take over values from persisted dao before saving
+     * @param <T>                  Domain class of the object which should be stored
+     * @param <S>                  Corresponding dao of the domain class
+     * @param <P>                  Dao class of the parent
+     * @return Stored domain object with additional generated ids, if missing before.
+     * <br>
+     * In case of not existing domain object for given identification, the result will be {@link Optional#empty()}
+     */
+    protected <T extends IIdentifiable, S extends IIdentifiableDao, P extends IIdentifiableDao> Optional<T> save(T domainObject
+            , String parentIdentification
+            , Nameable<T> nameable
+            , DaoCreator<P> parentDaoCreator
+            , DaoConverter<T, S, P> daoConverter
+            , DomainConverter<T, S> domainConverter
+            , JpaRepository<S, Long> repository
+            , Adoption<S> adoption) {
+        if (parentIdentification == null) {
+            log.error("There is no identification of the parent");
+            return Optional.empty();
+        }
+
+        String simpleClassName = domainObject.getClass().getSimpleName();
+
+        P parent = parentDaoCreator.create();
+        parent.setIdentification(parentIdentification);
+
+        S daoObject = daoConverter.convert(domainObject, parent);
+        T result;
+
+        if (domainObject.getIdentification() == null) {
+            log.debug("There is not any identification, so the {} with name {} will be stored the first time", simpleClassName, nameable.getName(domainObject));
+            daoObject = repository.save(daoObject);
+            result = domainConverter.convert(daoObject);
+            log.debug("The {} with name {} was stored with id {} and corresponding identification {}"
+                    , simpleClassName, nameable.getName(domainObject), daoObject.getId(), result.getIdentification());
+
+            return Optional.of(result);
+        }
+
+        Optional<S> storedDao = repository.findById(daoObject.getId());
+        if (storedDao.isEmpty()) {
+            log.debug("The {} with identification {} and id {} does not exists and could not be saved", simpleClassName, parentIdentification, daoObject.getId());
+            return Optional.empty();
+        }
+
+        daoObject = adoption.takeOver(daoObject, storedDao.get());
+
+        daoObject = repository.save(daoObject);
+        result = domainConverter.convert(daoObject);
+        log.debug("The {} with identification {} was saved", simpleClassName, result.getIdentification());
+
+        return Optional.of(result);
+    }
+
+    @FunctionalInterface
+    protected interface Nameable<T extends IIdentifiable> {
+        String getName(T t);
+    }
+
+    @FunctionalInterface
+    protected interface Adoption<T extends IIdentifiableDao> {
+        T takeOver(T toPersist, T persisted);
+    }
+
+    @FunctionalInterface
+    protected interface DaoCreator<T extends IIdentifiableDao> {
+        T create();
+    }
+
+    @FunctionalInterface
+    protected interface ConnectionCreator<P extends IIdentifiableDao, S extends IIdentifiableDao, T> {
+        T create(P parent, S child);
+    }
+
+    @FunctionalInterface
+    protected interface ConnectionDeleter<P extends IIdentifiableDao, S extends IIdentifiableDao> {
+        Long delete(P parent, S child);
+    }
+
+    @FunctionalInterface
+    protected interface DaoConverter<T extends IIdentifiable, S extends IIdentifiableDao, P extends IIdentifiableDao> {
+        S convert(T toConvert, P parent);
+    }
+
+    @FunctionalInterface
+    protected interface DomainConverter<T extends IIdentifiable, S extends IIdentifiableDao> {
+        T convert(S toConvert);
+    }
+}
