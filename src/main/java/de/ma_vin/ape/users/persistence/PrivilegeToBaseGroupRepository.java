3edd3582d5697a94d20e0099363f9e2f47e934f2
Ma-Vin <ma.vin.github@gmail.com>
2021-05-08 15:32:12 +0200

    bug: add missing deletion method at privilege to base group repository

--- src/main/java/de/ma_vin/ape/users/persistence/PrivilegeToBaseGroupRepository.java
+++ src/main/java/de/ma_vin/ape/users/persistence/PrivilegeToBaseGroupRepository.java
@@ -1,7 +1,11 @@
 package de.ma_vin.ape.users.persistence;
 
+import de.ma_vin.ape.users.model.gen.dao.group.PrivilegeGroupDao;
 import de.ma_vin.ape.users.model.gen.dao.group.PrivilegeGroupToBaseGroupDao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface PrivilegeToBaseGroupRepository extends JpaRepository<PrivilegeGroupToBaseGroupDao, PrivilegeGroupToBaseGroupDao.PrivilegeGroupToBaseGroupId> {
+    @Transactional
+    long deleteByPrivilegeGroup(PrivilegeGroupDao privilegeGroup);
 }
