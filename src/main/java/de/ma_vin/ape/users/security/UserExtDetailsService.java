735089a2cd64e30c49275149ed077468acad1119
Ma-Vin <ma.vin.github@gmail.com>
2021-05-17 20:05:32 +0200

    feature: add dummy security

---
+++ src/main/java/de/ma_vin/ape/users/security/UserExtDetailsService.java
@@ -0,0 +1,23 @@
+package de.ma_vin.ape.users.security;
+
+import de.ma_vin.ape.users.model.domain.user.UserExt;
+import de.ma_vin.ape.users.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserExtDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String userIdentification) throws UsernameNotFoundException {
+        return (UserExt) userService.findUser(userIdentification).orElseThrow(
+                () -> new UsernameNotFoundException(String.format("The User with identification %s does not exist", userIdentification))
+        );
+    }
+}
