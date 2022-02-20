d0f2fe6639db4606eb7d64acd9542e8b6d9846e0
Ma-Vin <ma.vin.github@gmail.com>
2022-02-20 14:33:53 +0100

    refactor #3: remove unused import

--- src/main/java/de/ma_vin/ape/users/controller/BaseGroupController.java
+++ src/main/java/de/ma_vin/ape/users/controller/BaseGroupController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "group/base")
