a558d35ad1780697755b85b596c5574514a732a5
Ma-Vin <ma.vin.github@gmail.com>
2022-08-20 14:51:36 +0200

    refactor #25: remove deprecated WebSecurityConfigurerAdapter

--- src/main/java/de/ma_vin/ape/users/security/SecurityConfig.java
+++ src/main/java/de/ma_vin/ape/users/security/SecurityConfig.java
@@ -4,17 +4,18 @@ import de.ma_vin.ape.users.properties.AuthClients;
 import de.ma_vin.ape.users.security.filter.ClientCheckFilter;
 import de.ma_vin.ape.users.security.service.ClientDetailService;
 import de.ma_vin.ape.users.security.service.UserExtDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -30,6 +31,15 @@ public class SecurityConfig {
     public static final String INTROSPECTION_PATTERN = "/oauth/introspection";
     public static final String CONSOLE_PATTERN = "/console/**";
     public static final String OAUTH_SECURED_REGEX = "\\/(group|user|admin)\\/.*";
+
+    @Value("${spring.security.oauth2.resourceserver.opaque.introspection-uri}")
+    private String introspectionUri;
+
+    @Value("${spring.security.oauth2.resourceserver.opaque.introspection-client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.resourceserver.opaque.introspection-client-secret}")
+    private String clientSecret;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -56,117 +66,82 @@ public class SecurityConfig {
         return source;
     }
 
-    @Configuration
-    @Order(5)
-    public static class OAuthAuthorizeConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public AuthenticationManager authManager(BCryptPasswordEncoder passwordEncoder
+            , UserExtDetailsService userDetailsService, ClientDetailService clientDetailService) {
 
-        @Autowired
-        private UserExtDetailsService userExtDetailsService;
+        DaoAuthenticationProvider clientAuthProvider = new DaoAuthenticationProvider();
+        clientAuthProvider.setPasswordEncoder(passwordEncoder);
+        clientAuthProvider.setUserDetailsService(clientDetailService);
 
-        @Autowired
-        private BCryptPasswordEncoder passwordEncoder;
+        DaoAuthenticationProvider userAuthProvider = new DaoAuthenticationProvider();
+        userAuthProvider.setPasswordEncoder(passwordEncoder);
+        userAuthProvider.setUserDetailsService(userDetailsService);
 
-        @Override
-        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            auth.userDetailsService(userExtDetailsService).passwordEncoder(passwordEncoder);
+        return new ProviderManager(clientAuthProvider, userAuthProvider);
+    }
+
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain oAuthTokenFilterChain(HttpSecurity http, AuthClients authClients) throws Exception {
+        if (authClients.isTokenWithClientSecret()) {
+            http.antMatcher(TOKEN_PATTERN).authorizeRequests().anyRequest().authenticated()
+                    .and().httpBasic();
+        } else {
+            http.antMatcher(TOKEN_PATTERN).authorizeRequests().anyRequest().permitAll();
         }
+        http.cors().and().csrf().disable();
 
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().antMatchers(AUTHORIZE_PATTERN).authenticated()
-                    .and().cors()
-                    .and().formLogin();
-        }
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain introspectionFilterChain(HttpSecurity http) throws Exception {
+        http.antMatcher(INTROSPECTION_PATTERN).authorizeRequests().anyRequest().authenticated()
+                .and().cors()
+                .and().httpBasic()
+                .and().csrf().disable();
+
+        return http.build();
+    }
+
+
+    @Bean
+    @Order(3)
+    public SecurityFilterChain oAuthResourceFilterChain(HttpSecurity http) throws Exception {
+        http.regexMatcher(OAUTH_SECURED_REGEX).authorizeRequests().anyRequest().authenticated().and()
+                .oauth2ResourceServer(
+                        oauth2 -> oauth2.opaqueToken(token -> token.introspectionUri(this.introspectionUri)
+                                .introspectionClientCredentials(this.clientId, this.clientSecret)
+                        )
+                )
+                .cors();
+
+        return http.build();
     }
 
     /*
-    @Configuration
-    @Order(4)*/
-    public static class H2ConsoleConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().antMatchers(CONSOLE_PATTERN).permitAll()
-                    .and().csrf().disable()
-                    .headers().frameOptions().disable();
-        }
+    @Bean
+    @Order(4)
+    */
+    public SecurityFilterChain h2ConsoleFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers(CONSOLE_PATTERN).permitAll()
+                .and().csrf().disable()
+                .headers().frameOptions().disable();
+
+        return http.build();
     }
 
-    @Configuration
-    @Order(1)
-    public static class OAuthTokenConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    @Order(5)
+    public SecurityFilterChain oAuthAuthorizeFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers(AUTHORIZE_PATTERN).authenticated()
+                .and().cors()
+                .and().formLogin();
 
-        @Autowired
-        AuthClients authClients;
-
-        @Autowired
-        private ClientDetailService clientDetailService;
-
-        @Autowired
-        private BCryptPasswordEncoder passwordEncoder;
-
-        @Override
-        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            auth.userDetailsService(clientDetailService).passwordEncoder(passwordEncoder);
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            if (authClients.isTokenWithClientSecret()) {
-                http.antMatcher(TOKEN_PATTERN).authorizeRequests().anyRequest().authenticated()
-                        .and().httpBasic();
-            } else {
-                http.antMatcher(TOKEN_PATTERN).authorizeRequests().anyRequest().permitAll();
-            }
-            http.cors().and().csrf().disable();
-        }
+        return http.build();
     }
 
-    @Configuration
-    @Order(2)
-    public static class IntrospectionConfig extends WebSecurityConfigurerAdapter {
-
-        @Autowired
-        private ClientDetailService clientDetailService;
-
-        @Autowired
-        private BCryptPasswordEncoder passwordEncoder;
-
-        @Override
-        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            auth.userDetailsService(clientDetailService).passwordEncoder(passwordEncoder);
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.antMatcher(INTROSPECTION_PATTERN).authorizeRequests().anyRequest().authenticated()
-                    .and().cors()
-                    .and().httpBasic()
-                    .and().csrf().disable();
-        }
-    }
-
-    @Configuration
-    @Order(3)
-    public static class OAuthResourceConfig extends WebSecurityConfigurerAdapter {
-
-        @Value("${spring.security.oauth2.resourceserver.opaque.introspection-uri}")
-        private String introspectionUri;
-
-        @Value("${spring.security.oauth2.resourceserver.opaque.introspection-client-id}")
-        private String clientId;
-
-        @Value("${spring.security.oauth2.resourceserver.opaque.introspection-client-secret}")
-        private String clientSecret;
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.regexMatcher(OAUTH_SECURED_REGEX).authorizeRequests().anyRequest().authenticated().and()
-                    .oauth2ResourceServer(
-                            oauth2 -> oauth2.opaqueToken(token -> token.introspectionUri(this.introspectionUri)
-                                    .introspectionClientCredentials(this.clientId, this.clientSecret)
-                            )
-                    )
-                    .cors();
-        }
-    }
 }
